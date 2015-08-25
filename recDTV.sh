cdb0bcd154b9f306f1f3cd545e6416a693a9b6cd
sf44tdw <prmkaddr@yahoo.co.jp>
2015-08-25 12:07:43 +0900

    44

--- recDTV.sh
+++ recDTV.sh
@@ -1,7 +1,19 @@
 ﻿#!/bin/bash
 
-DATE=`date "+%Y%m%d%H%M%S"`
+#チャンネル番号(入力)
+Ch=${1}
 
-filename="D"${DATE}"P"$$
+#録画秒数(入力)
+Time=${2}
 
-echo ${filename}
+#番組名取得
+Title=`programmeGetter.sh ${1}`
+
+#日付取得
+Date=`date "+%Y%m%d%H%M%S"`
+
+#ファイル名生成
+filename=${Title}_"D"${Date}"P"$$"T"${Time}
+
+echo ${FileName}
+
