79a01529171a9e8eb548082db8ada8f906963731
sf44tdw <prmkaddr@yahoo.co.jp>
2015-08-25 12:20:39 +0900

    ddd

--- recDTV.sh
+++ recDTV.sh
@@ -2,18 +2,22 @@
 
 #チャンネル番号(入力)
 Ch=${1}
+echo ${Ch}
 
 #録画秒数(入力)
 Time=${2}
+echo ${Time}
 
 #番組名取得
 Title=`programmeGetter.sh ${1}`
+echo ${Title}
 
 #日付取得
 Date=`date "+%Y%m%d%H%M%S"`
+echo ${Date}
 
 #ファイル名生成
-filename=${Title}_"D"${Date}"P"$$"T"${Time}
+FileName=${Title}_"D"${Date}"P"$$"T"${Time}
 
 echo ${FileName}
 
